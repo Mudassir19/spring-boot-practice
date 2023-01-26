b006c5f93ee2ab97657b7ac9292ac7d7b9d38ac8
Mudassir Shaikh <mudassir.shaikh@atos.net>
2023-01-26 12:27:17 +0530

    code

--- src/main/java/com/practice/boot/BootApplication.java
+++ src/main/java/com/practice/boot/BootApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 //@EnableJpaRepositories
 public class BootApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BootApplication.class, args);
 
